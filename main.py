93beaa5fc0cbe5f07e40197fd013f8d108886cb2
josegarban <josegarban@gmail.com>
2023-02-08 17:21:43 +0100

    Función lista_ficheros tiene el comportamiento deseado.

--- main.py
+++ main.py
@@ -1,35 +1,38 @@
 import os
 from pprint import pprint
 
-def lista_ficheros  (ruta_inicial=os.path.dirname(os.path.realpath(__file__)), carpeta="genomas"):
+def lista_ficheros (ruta_inicial=os.path.dirname(os.path.realpath(__file__)), carpeta="genomas"):
     """
     Proveer una lista de los ficheros
     :param ruta_inicial: carpeta donde se explorarán los ficheros. El valor predeterminado es la carpeta inicial donde está el script.
-    :return: diccionario con la estructura de los ficheros
+    :return: lista con dos elementos: la lista de subcarpetas y la lista de ficheros .txt
+    Se presupone que en las carpetas y subcarpetas hay sólo ficheros .txt
     """
-    # Diccionario vacío que contendrá los ficheros
-    lista_ficheros = list()
-    # Booleanos para saber si es una subcarpeta vacía o si contiene ficheros .txt
-    subcarpeta_txt      = False
-    subcarpeta_vacia    = True
+    # Lista vacía que contendrá las subcarpetas y los ficheros
+    output = [None, None]
 
     # Hallar el directoio actual y concatenar la carpeta
     ruta = ruta_inicial + "/" + carpeta
 
     # Hallamos la lista de ficheros en la subcarpeta, así como los que sean .txt (los que vamos a analizar)
     print("Lista de subcarpetas en la carpeta {0}".format(carpeta))
-    lista_ficheros  = os.listdir(ruta)
-    lista_txt       = [x for x in lista_ficheros if ".txt" in x]
+    lista               = os.listdir(ruta)
+    lista_subcarpetas   = [ruta_inicial + "\\" + x for x in lista if not os.path.isfile(x)]
+    lista_txt           = [ruta_inicial + "\\" + x for x in lista if ".txt" in x]
 
-    # Llenar los booleanos
-    if len(lista_txt) == 0:
-        subcarpeta_vacia = True
-    if len(lista_txt) == len(lista_ficheros):
-        subcarpeta_txt = True
+    output[0], output[1] = lista_subcarpetas, lista_txt
+    pprint(output)
+    return output
 
-    pprint(lista_ficheros)
+def main (ruta_inicial=os.path.dirname(os.path.realpath(__file__)), carpeta="genomas"):
+    """
+    Explorar todo el árbol de carpetas
+    :param ruta_inicial: inicio donde están todas las subcarpetas y ficheros a explorar
+    :param carpeta: carpeta donde se van a buscar los ficheros
+    :return:
+    """
 
 if __name__ == "__main__":
     print("Corriendo script...")
-    lista_ficheros()
+    main()
 
