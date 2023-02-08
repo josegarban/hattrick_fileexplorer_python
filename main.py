52aa4cb3e543fd0a3fc7d10fd53c90a43282c272
josegarban <josegarban@gmail.com>
2023-02-08 18:56:43 +0100

    MVP delivered.

--- main.py
+++ main.py
@@ -1,38 +1,65 @@
 import os
 from pprint import pprint
 
-def lista_ficheros (ruta_inicial=os.path.dirname(os.path.realpath(__file__)), carpeta="genomas"):
+def estructura_completa(ruta_inicial=os.path.dirname(os.path.realpath(__file__))):
     """
-    Proveer una lista de los ficheros
-    :param ruta_inicial: carpeta donde se explorarán los ficheros. El valor predeterminado es la carpeta inicial donde está el script.
-    :return: lista con dos elementos: la lista de subcarpetas y la lista de ficheros .txt
-    Se presupone que en las carpetas y subcarpetas hay sólo ficheros .txt
+    Retorna una lista con todos los ficheros en las carpetas que estén dentro de ruta_inicial
+    :param ruta_inicial: directorio raíz donde se explorará
+    :return: lista de ficheros
     """
-    # Lista vacía que contendrá las subcarpetas y los ficheros
-    output = [None, None]
+    ficheros, ficheros_normalizados = [], []
+    for root, dirs, files in os.walk(ruta_inicial):
+        ficheros = ficheros + [root + "\\" + f for f in files]
 
-    # Hallar el directoio actual y concatenar la carpeta
-    ruta = ruta_inicial + "/" + carpeta
+    ficheros_relativos = [f.replace(os.path.dirname(os.path.realpath(__file__))+"\\", "") for f in ficheros]
 
-    # Hallamos la lista de ficheros en la subcarpeta, así como los que sean .txt (los que vamos a analizar)
-    print("Lista de subcarpetas en la carpeta {0}".format(carpeta))
-    lista               = os.listdir(ruta)
-    lista_subcarpetas   = [ruta_inicial + "\\" + x for x in lista if not os.path.isfile(x)]
-    lista_txt           = [ruta_inicial + "\\" + x for x in lista if ".txt" in x]
+    return ficheros_relativos
 
-    output[0], output[1] = lista_subcarpetas, lista_txt
-    pprint(output)
-    return output
+def leer_txt(fichero_entrada):
+    """
+    Lee un fichero de texto y lo convierte en una cadena de texto
+    :param fichero_entrada: ruta del fichero (relativa)
+    :return: cadena de texto de lo que haya en el fichero
+    """
+    with open(fichero_entrada, "r") as f:
+        lineas = f.readlines()
+        texto = "".join(lineas)
+    return texto
 
-def main (ruta_inicial=os.path.dirname(os.path.realpath(__file__)), carpeta="genomas"):
+def mostrar_ficheros_relevantes(lista_ficheros, cadena):
+    """
+    Muestra los ficheros que contengan la cadena de texto cadena
+    :param lista_ficheros: lista de ficheros a explorar
+    :param cadena: cadena
+    :return: lista de ficheros
+    """
+    # Lista de ficheros .txt con el texto relevante
+    lista_ficheros_relevantes = []
+    for f in lista_ficheros:
+        texto = leer_txt(f)
+        if cadena in texto:
+            lista_ficheros_relevantes.append("\n" + f + "\n" + texto)
+    return lista_ficheros_relevantes
+
+def buscar (carpeta="genomas", cadena="wax synthase"):
     """
     Explorar todo el árbol de carpetas
-    :param ruta_inicial: inicio donde están todas las subcarpetas y ficheros a explorar
     :param carpeta: carpeta donde se van a buscar los ficheros
+    :param cadena: texto a buscar
     :return:
     """
 
+    # Hallar el directoio actual y concatenar la carpeta
+    ruta = os.path.dirname(os.path.realpath(__file__)) + "\\" + carpeta
+    # Mostrar estructura completa
+    ficheros_todos = estructura_completa(ruta)
+    # Leer todos los ficheros y buscar el texto especificado
+    ficheros_relevantes = mostrar_ficheros_relevantes(ficheros_todos, cadena)
+    # Mostrar ficheros
+    print(ficheros_relevantes)
+
+    return ficheros_relevantes
+
 if __name__ == "__main__":
     print("Corriendo script...")
-    main()
-
+    output = buscar()
